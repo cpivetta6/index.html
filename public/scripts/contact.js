d132e55b1ad12e89f4c0cab1e6917edca4d5751f
cpivetta6 <caiopivetta6@gmail.com>
2023-08-28 09:37:33 +0200

    change event click

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,37 +1,37 @@
 function sendEmail() {
   var params = {
-    from_name: document.getElementById("fullname").value, // Fixed typo: "fullName" should be "fullname"
-    email_id: document.getElementById("email_id").value,
+    from_name: document.getElementById("fullname").value,
+    email_id: "",
     message: document.getElementById("message").value,
   };
 
-  var clearButton = document
-    .getElementById("clearButton")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      var emailValue = document.getElementById("email_id").value;
-      var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  var emailValue = document.getElementById("email_id").value;
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  var valid = false;
 
-      if (emailValue !== "") {
-        var valid = emailPattern.test(emailValue);
-        console.log(valid);
+  console.log(params);
+  console.log(emailValue);
 
-        if (valid) {
-          // Email is valid, perform the email sending actions
-          console.log(params);
-          emailjs
-            .send("service_a1k7qtd", "template_1uyimi5", params)
-            .then(function (res) {
-              alert("Message Sent: " + res.status);
-            });
+  if (emailValue !== "") {
+    if (emailPattern.test(emailValue)) {
+      valid = true;
+      console.log(valid);
 
-          document.getElementById("fullname").value = "";
-          document.getElementById("email_id").value = "";
-          document.getElementById("message").value = "";
-        } else {
-          // Email is invalid
-          alert("Invalid email format");
-        }
-      }
-    });
+      params.email_id = emailValue;
+
+      emailjs
+        .send("service_a1k7qtd", "template_1uyimi5", params)
+        .then(function (res) {
+          alert("Message Sent: " + res.status);
+        });
+
+      document.getElementById("fullname").value = "";
+      document.getElementById("email_id").value = "";
+      document.getElementById("message").value = "";
+    } else {
+      console.log(valid);
+      // Email is invalid
+      alert("Invalid email format");
+    }
+  }
 }
